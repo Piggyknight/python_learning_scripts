a7a12fc66bb25003db86c9b51055db3d1487fb1e
michael <michael@leshugame.com>
2018-08-07 17:33:14 +0800

    update doc

--- weekly_report_collector/last_week_finished_report.py
+++ weekly_report_collector/last_week_finished_report.py
@@ -1,35 +1,58 @@
 # -*- coding:utf-8 -*-
-_debug_format =  '[%s]%s (%s)\n'
+import re
+_debug_format =  '[%s]%s (%s/%s) \n'
 _week_report_format = "[%s]%s (%s, %s)\n"
 
 '''
 store row data of last week finished task data
+- 暂不支持单位的转换, 统一使用天数(d)为单位
 '''
 class LastWeekFinishedTaskRow:
     def __init__(self):
         self._module_str = ''
         self._task_str = ''
-        self._duration = ''
+        self._duration = 0
+        self._duration_str = ''
         self._owner=''
 
+
     def safe_check_time(self, time):
+        '''
+        保障时间字符串包含单位符号天数(d)
+        '''
         time_lower = str(time).lower()
         if (time_lower.count('h') == 0 and
                     time_lower.count('d') == 0):
             return time_lower + 'd'
         return time
 
+    def _convert_time(self, time):
+        '''
+        将时间转换成int
+        '''
+        time_str=str(time).lower()
+        replace_str='[dh]'
+        time_str = re.sub(replace_str, '', time_str)
+        return int(time_str)
+        
     def ReadData(self, row):
         self._module_str = row[0]
         if row[1] != '':
             self._task_str = row[1].replace('\n', ',')
-        self._duration = self.safe_check_time(row[2])
+        self._duration_str = self.safe_check_time(row[2])
+        self._duration = self._convert_time(row[2])
 
     def __str__(self):
-        return  _debug_format % (self._module_str, self._task_str, self._duration)
+        return  _debug_format % (self._module_str, 
+                                self._task_str,
+                                self._duration_str,
+                                self._duration)
 
     def __repr__(self):
-        return _debug_format % (self._module_str, self._task_str, self._duration)
+        return _debug_format % (self._module_str,
+                                self._task_str,
+                                self._duration_str,
+                                self._duration)
 
 
 '''
--- weekly_report_collector/unittest_weeklyreport.py
+++ weekly_report_collector/unittest_weeklyreport.py
@@ -3,9 +3,9 @@
 from report_loader_xlsx import *
 from xls_collector import *
 from xls_week_report import *
-from week_task_report import *
-from last_week_finished_report import *
-from last_week_unfinished_report import *
+from week_task_report import WeekTaskRow, ExportWeekTaskRow
+from last_week_finished_report import LastWeekFinishedTaskRow, ExportLastWeekFinishedTaskRow
+from last_week_unfinished_report import LastWeekUnFinishedTaskRow, ExportLastWeekUnFinishedTaskRow
 from report_24_category import *
 from report_24_exporter import *
 from project_calendar import *
@@ -24,7 +24,11 @@ def unit_test_read_form():
 
 def unit_test_all_report():
     dict = AnalyzeXlsx(xlsx_data, keys)
-    objs = [WeekTaskRow(), LastWeekFinishedTaskRow(), LastWeekUnFinishedTaskRow()]
+    objs = [
+            WeekTaskRow(),
+            LastWeekFinishedTaskRow(),
+            LastWeekUnFinishedTaskRow()
+            ]
     for key, obj in zip(keys, objs):
         week_report_data = ReadForm(xlsx_data, dict[key])
         week_report = WeekReport()
@@ -33,10 +37,19 @@ def unit_test_all_report():
 
 
 #_folder = 'D:\\Project\\Git\\python_learning_scripts\\weekly_report_collector\\unit_test'
-_folder = 'd:\\summer_plan\\week_report\\20180717\\'
+_folder = 'd:\\summer_plan\\week_report\\20180807\\'
 
-_exporter = [ExportWeekTaskRow(), ExportLastWeekFinishedTaskRow(), ExportLastWeekUnFinishedTaskRow()]
-_rows = [WeekTaskRow(), LastWeekFinishedTaskRow(), LastWeekUnFinishedTaskRow()]
+_exporter = [
+            ExportWeekTaskRow(),
+            ExportLastWeekFinishedTaskRow(),
+            ExportLastWeekUnFinishedTaskRow()
+            ]
+            
+_rows = [
+        WeekTaskRow(), 
+        LastWeekFinishedTaskRow(),
+        LastWeekUnFinishedTaskRow()
+        ]
 
 def unit_test_SearchFiles_and_GetInfo():
     allFiles = SearchFiles(_folder)
@@ -48,22 +61,32 @@ def unit_test_SearchFiles_and_GetInfo():
 
 def unit_test_export_report():
     report = [list(), list(), list()]
+
+    # 在目录中找到所有文件
     allFiles = SearchFiles(_folder)
     time = ''
 
+    
     for file in allFiles:
+        # 从文件名解析相应信息, 范例: 周报_20180705_henry
+        #   - info[0] : 时间
+        #   - info[1] : name
         info = GetInfoFromFileName(file)
         time = info[0]
 
+        # 获取xlsx的数据, 转换成单独的表存于dict中
         print('Reading '+ info[1] + ' report' )
         xlsx_data = ReadXlsx(file)
         dict = AnalyzeXlsx(xlsx_data, keys)
+
+        # 讲数据全部存于相对应的exporter中
         for i in range(0, len(_exporter)):
             week_report_data = ReadForm(xlsx_data, dict[keys[i]])
             week_report = WeekReport()
             week_report.ReadData(info[1],week_report_data, _rows[i].__class__)
             report[i].append(week_report.Export(_exporter[i]))
 
+    # 遍历所有exporter并导出
     fw = open('D:\\report.txt', 'w',encoding='utf-8')
     for i in range(0, len(_exporter)):
         title_str = '##%s\n' % keys[i]
@@ -72,6 +95,8 @@ def unit_test_export_report():
             for str in strs:
                 fw.write(str)
         fw.write('\n\n\n')
+
+    return
 
 
 _calendar_ini_file = 'D:\\Project\\Git\\python_learning_scripts\\weekly_report_collector\\calendar_cnf.ini'
@@ -120,8 +145,7 @@ def unit_test_export_24_report():
 
     #export file
     exporter.ExportReport(report_list, 'D:\\24_report.txt')
-
-
+    return
 
 def unity_test_ProjectCalendar():
     file= 'D:\\Project\\Git\\python_learning_scripts\\weekly_report_collector\\calendar_cnf.ini'
@@ -131,7 +155,7 @@ def unity_test_ProjectCalendar():
     print(pc.GetIsoTime('W3.1'))
     print(pc.GetIsoTime('w1.1'))
     print(pc.GetIsoTime('w4.6'))
-
+    return
 
 def unit_test_load_report_ini():
     cf = Report24Category()
@@ -142,6 +166,7 @@ def unit_test_load_report_ini():
     print(cf.GetCategory('好友'))
     print(cf.GetCategory('百环'))
     print(cf.GetCategory('NPC|Entity|Monster'))
+    return
 
 
 #unit_test_export_24_report()
